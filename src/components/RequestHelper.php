2360b4fdbd9aa9209c8d6b9babfdb8c017157566
Algirdas Gurevicius <a.gurevicius@gmail.com>
2021-02-19 10:59:51 +0200

    renamed methods to set added setHeader() method

--- src/components/RequestHelper.php
+++ src/components/RequestHelper.php
@@ -17,56 +17,56 @@
             $this->host = $host;
         }
 
-        public function addAccept( $value = '*/*' ) {
+        public function setAccept( $value = '*/*' ) {
 
             $this->headers['Accept'] = $value;
             return $this;
         }
 
-        public function addAcceptAsHtml() {
+        public function setAcceptAsHtml() {
 
-            return $this->addAccept( 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9' );
+            return $this->setAccept( 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9' );
         }
 
-        public function addAcceptEncoding( $value = 'gzip, deflate, br' ) {
+        public function setAcceptEncoding( $value = 'gzip, deflate, br' ) {
 
             $this->headers['Accept-Encoding'] = $value;
             return $this;
         }
 
-        public function addAcceptLanguage( $value = 'en-US,en;q=0.9,lt-LT;q=0.8,lt;q=0.7' ) {
+        public function setAcceptLanguage( $value = 'en-US,en;q=0.9,lt-LT;q=0.8,lt;q=0.7' ) {
 
             $this->headers['Accept-Language'] = $value;
             return $this;
         }
 
-        public function addConnection( $value = 'keep-alive' ) {
+        public function setConnection( $value = 'keep-alive' ) {
 
             $this->headers['Connection'] = $value;
             return $this;
         }
 
-        public function addCacheControl( $value = 'no-cache' ) {
+        public function setCacheControl( $value = 'no-cache' ) {
 
             $this->headers['Cache-Control'] = $value;
             $this->headers['Pragma'] = $value;
             return $this;
         }
 
-        public function addContentType( $value = 'application/x-www-form-urlencoded' ) {
+        public function setContentType( $value = 'application/x-www-form-urlencoded' ) {
 
             $this->headers['Content-Type'] = $value;
             return $this;
         }
 
-        public function addHost() {
+        public function setHost() {
 
             $this->headers['Host'] = $this->host;
             $this->headers['Origin'] = 'https://' . $this->host;
             return $this;
         }
 
-        public function addSec(
+        public function setSec(
             $ua = '"Chromium";v="88", "Google Chrome";v="88", ";Not A Brand";v="99"',
             $ua_mobile = '?0',
             $document = 'document',
@@ -90,13 +90,13 @@
             return $this;
         }
 
-        public function addUpgradeInsecureRequests( $value = '1' ) {
+        public function setUpgradeInsecureRequests( $value = '1' ) {
 
             $this->headers['Upgrade-Insecure-Requests'] = $value;
             return $this;
         }
 
-        public function addUserAgent( $value = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36' ) {
+        public function setUserAgent( $value = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36' ) {
 
             $this->headers['User-Agent'] = $value;
             return $this;
@@ -105,17 +105,23 @@
         public static function createDefaultHtmlRequest( $host ) {
 
             return ( new self( $host ) )
-                ->addAcceptAsHtml()
-                ->addAcceptEncoding()
-                ->addAcceptLanguage()
-                ->addCacheControl()
-                ->addConnection()
-                ->addContentType()
-                ->addHost()
-                ->addSec()
-                ->addUpgradeInsecureRequests()
-                ->addUserAgent();
+                ->setAcceptAsHtml()
+                ->setAcceptEncoding()
+                ->setAcceptLanguage()
+                ->setCacheControl()
+                ->setConnection()
+                ->setContentType()
+                ->setHost()
+                ->setSec()
+                ->setUpgradeInsecureRequests()
+                ->setUserAgent();
 
+        }
+
+        public function setHeader( $header, $value ) {
+
+            $this->headers[ $header ] = $value;
+            return $this;
         }
 
         public function toArray() {
